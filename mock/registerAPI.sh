c7becf229aed9bf7376aea01525ae4dad9fced24
Wojciech Trocki <wtrocki@redhat.com>
2021-01-15 12:44:08 +0000

    fix: invalid arguments in mock

--- mock/registerAPI.sh
+++ mock/registerAPI.sh
@@ -1,7 +1,7 @@
 echo "Registering CRD's on cluster"
 
-oc apply -f -n openshift ./crds/ManagedKafkaConnection.yml       
-oc apply -f -n openshift ./crds/ManagedKafkaRequest.yml          
-oc apply -f -n openshift ./crds/ManagedServiceAccountRequest.yml
+oc apply -n openshift -f ./crds/ManagedKafkaConnection.yml       
+oc apply -n openshift -f  ./crds/ManagedKafkaRequest.yml          
+oc apply -n openshift -f  ./crds/ManagedServiceAccountRequest.yml
 
 echo "Plugin can be used now. Create ManagedKafkaRequest CR"
