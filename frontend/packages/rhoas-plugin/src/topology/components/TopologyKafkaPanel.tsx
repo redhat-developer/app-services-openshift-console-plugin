d2d67188a386a5457fff2e03af792cd0b1d385a4
Wojciech Trocki <wtrocki@redhat.com>
2021-02-11 17:09:12 +0000

    fix: issure with secret

--- frontend/packages/rhoas-plugin/src/topology/components/TopologyKafkaPanel.tsx
+++ frontend/packages/rhoas-plugin/src/topology/components/TopologyKafkaPanel.tsx
@@ -58,14 +58,17 @@ const DetailsComponent: React.FC<any> = ({ obj }) => {
 };
 
 const ResourcesComponent = ({ obj }) => {
-  const accessTokenSecretName = obj.spec.accessTokenSecretName;
+  const serviceAccountSecretName = obj?.spec?.credentials?.serviceAccountSecretName;
   const { namespace } = obj.metadata;
   const link = (
-    <ResourceLink
-      kind={referenceForModel(SecretModel)}
-      name={accessTokenSecretName}
-      namespace={namespace}
-    />
+    <>
+      <h3>Secret</h3>
+      <ResourceLink
+        kind={referenceForModel(SecretModel)}
+        name={serviceAccountSecretName}
+        namespace={namespace}
+      />
+    </>
   );
 
   return (
