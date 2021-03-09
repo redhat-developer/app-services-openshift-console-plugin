945e83222466a5518365bd67238138f683e1c0b5
Wojtek Trocki <wtrocki@redhat.com>
2021-03-09 17:45:11 +0000

    fix: add extra handling for MSAR (#115)

--- frontend/packages/rhoas-plugin/src/utils/conditionHandler.ts
+++ frontend/packages/rhoas-plugin/src/utils/conditionHandler.ts
@@ -1,12 +1,11 @@
-import { StatusCondition } from "../types/rhoas-types";
+import { StatusCondition } from './rhoas-types';
 
-export const getFinishedCondition = (request: any) => {
-  return getCondition(request, "Finished");
-}
+export class ResourceConditionError extends Error {
+  public conditionContext: StatusCondition[];
 
-export const isSuccessfull = (request: any) => {
-  const condition = getCondition(request, "Finished");
-  return condition && condition.status === "True"
+  public isTokenValid: boolean;
+
+  public isGenericProblem: boolean;
 }
 
 export const getCondition = (request: any, name: string) => {
@@ -17,4 +16,14 @@ export const getCondition = (request: any, name: string) => {
       }
     }
   }
-}
+  return undefined;
+};
+
+export const getFinishedCondition = (request: any) => {
+  return getCondition(request, 'Finished');
+};
+
+export const isSuccessfull = (request: any) => {
+  const condition = getCondition(request, 'Finished');
+  return condition && condition.status === 'True';
+};
