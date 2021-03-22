a6c3a7ccbdad5a542fde266d9cc8fcddbe38435f
Wojciech Trocki <wtrocki@redhat.com>
2021-03-22 09:15:36 +0000

    fix: remove unnedded load error

--- frontend/packages/rhoas-plugin/src/catalog/providers/useRhoasCatalog.tsx
+++ frontend/packages/rhoas-plugin/src/catalog/providers/useRhoasCatalog.tsx
@@ -23,7 +23,7 @@ import { CATALOG_TYPE } from '../const';
 const useRhoasCatalog: CatalogExtensionHook<CatalogItem[]> = (): [CatalogItem[], boolean, any] => {
   const [currentNamespace] = useActiveNamespace();
   const { t } = useTranslation();
-  const [serviceAccount, loaded, loadError] = useK8sWatchResource({
+  const [serviceAccount, loaded] = useK8sWatchResource({
     kind: referenceForModel(CloudServiceAccountRequest),
     isList: false,
     name: ServiceAccountCRName,
@@ -35,7 +35,7 @@ const useRhoasCatalog: CatalogExtensionHook<CatalogItem[]> = (): [CatalogItem[],
     return [[], false, undefined];
   }
 
-  const isServiceAccountValid = loadError || isResourceStatusSuccessfull(serviceAccount);
+  const isServiceAccountValid = isResourceStatusSuccessfull(serviceAccount);
 
   const tokenStatusFooter = () => {
     let token;
