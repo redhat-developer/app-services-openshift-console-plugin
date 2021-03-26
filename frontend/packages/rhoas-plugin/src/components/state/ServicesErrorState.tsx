e8fd95fd258e6e2d04e0f2b8e9c8a8f0862f6c1a
Wojciech Trocki <wtrocki@redhat.com>
2021-03-26 10:36:15 +0000

    fix: unify empty and error states

---
+++ frontend/packages/rhoas-plugin/src/components/state/ServicesErrorState.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import ErrorCircle from '@patternfly/react-icons/dist/js/icons/error-circle-o-icon';
+import './ServicesErrorState.css';
+import { ServicesEmptyState, ServicesEmptyStateProps } from './ServicesEmptyState';
+
+export const ServicesErrorState = ({ title, message, actionLabel }: ServicesEmptyStateProps) => {
+  return (
+    <ServicesEmptyState
+      title={title}
+      message={message}
+      actionLabel={actionLabel}
+      icon={ErrorCircle}
+      iconClass={"rhoas-services-error-state-icon"}
+    ></ServicesEmptyState>
+  );
+};
